5aa9c09d59a97e6613e9bd327a47dc202051853f
Silvia Calcaterra <silvia.calcaterra@hotmail.it>
2021-07-14 11:33:52 +0200

    heroku setup

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ const generateId = () => {
   return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER / 4)
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
